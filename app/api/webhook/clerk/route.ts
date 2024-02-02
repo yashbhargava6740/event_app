0226f7a2fde01107925e2c040699479596cf6d60
Ncoder <bhargavayash2015@gmail.com>
2024-02-03 02:36:37 +0530

    changed_req_to_text

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -29,9 +29,9 @@ export async function POST(req: Request) {
   }
  
   // Get the body
-  const payload = await req.json()
-  const body = JSON.stringify(payload);
- 
+  // const payload = await req.json()
+  // const body = JSON.stringify(payload);
+  const body = await req.text();
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
